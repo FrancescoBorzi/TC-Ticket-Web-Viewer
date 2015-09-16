eba2263bca966ac0ce0ad9620bff831858ad7012
ShinDarth <borzifrancesco@gmail.com>
2015-09-16 02:55:14 +0200

    Improved output + added screenshot

--- js/app/viewer.js
+++ js/app/viewer.js
@@ -1,4 +1,8 @@
+/*jslint browser: true, white: true, plusplus: true */
+/*global angular, console, alert*/
+
 (function () {
+  'use strict';
   var app = angular.module('status', ['ui.bootstrap', 'chieffancypants.loadingBar', 'tableSort']);
 
   app.controller("StatusController", function($scope, $http) {
@@ -10,6 +14,7 @@
     $http.get( request )
       .success(function(data, status, header, config) {
 
+      $scope.apiLoaded = true;
       $scope.ticketsCount = data.length;
 
       if ($scope.ticketsCount > 0) {
@@ -17,9 +22,10 @@
       }
     })
       .error(function(data, status, header, config) {
-      console.log("Error while retrieving tickets.");
+      $scope.apiLoaded = false;
+      console.log("Error while retrieving tickets, API request failed: " + app.api + "search/tickets?closedBy=0");
     });
 
   });
 
-})()
+}());
