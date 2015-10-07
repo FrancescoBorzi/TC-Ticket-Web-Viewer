b553f3ac7a8ea751f73ef75cd9ef0c2ce66039b9
ShinDarth <borzifrancesco@gmail.com>
2015-10-07 03:21:24 +0200

    Implemented topGM stats

--- js/app/viewer.js
+++ js/app/viewer.js
@@ -9,10 +9,15 @@
 
     $scope.serverName = app.serverName;
 
-    var request = app.api + "search/tickets?unresolved=1";
-    $scope.apiLoaded = true;
+    var request1 = app.api + "search/tickets?unresolved=1",
+        request2 = app.api + "topgm/ticket/account",
+        request3 = app.api + "topgm/ticket/character";
 
-    $http.get( request )
+    $scope.apiLoaded = true;
+    $scope.showTopGMbyAccount   = false;
+    $scope.showTopGMbyCharacter = false;
+
+    $http.get( request1 )
       .success(function(data, status, header, config) {
 
       $scope.ticketsCount = data.length;
@@ -20,10 +25,28 @@
       if ($scope.ticketsCount > 0) {
         $scope.tickets = data;
       }
+
+      $http.get( request2 )
+        .success(function(data, status, header, config) {
+        $scope.topGMbyAccount = data;
+        if (data.length > 0) { $scope.showTopGMbyAccount = true; }
+      })
+        .error(function(data, status, header, config) {
+        console.log("Error while retrieving tickets, API request failed: " + request2);
+      });
+
+      $http.get( request3 )
+        .success(function(data, status, header, config) {
+        $scope.topGMbyCharacter = data;
+        if (data.length > 0) { $scope.showTopGMbyCharacter = true; }
+      })
+        .error(function(data, status, header, config) {
+        console.log("Error while retrieving tickets, API request failed: " + request3);
+      });
     })
       .error(function(data, status, header, config) {
       $scope.apiLoaded = false;
-      console.log("Error while retrieving tickets, API request failed: " + app.api + "search/tickets?unresolved=1");
+      console.log("Error while retrieving tickets, API request failed: " + request1);
     });
 
   });
